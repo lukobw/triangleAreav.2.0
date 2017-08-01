73a807bf7a182e56d951b57c10279a83906eed75
Lucas <viper20@poczta.fm>
2017-08-01 20:45:38 +0200

    value minus fixed

--- js.js
+++ js.js
@@ -1,13 +1,13 @@
 var triangle1Area = getTriangleArea(12,16);
-var triangle2Area = getTriangleArea(4,20);
+var triangle2Area = getTriangleArea(-4,20);
 var triangle3Area = getTriangleArea(5,11);
 
 function getTriangleArea(a, h){
-  if (a <= 0 || h <= 0) {
+  if (a === 0 || h === 0) {
     console.log ('Nieprawidłowe dane');
   } else {
     return (a * h / 2);
   }
 };
 
-console.log(triangle3Area);
+console.log(triangle2Area);
